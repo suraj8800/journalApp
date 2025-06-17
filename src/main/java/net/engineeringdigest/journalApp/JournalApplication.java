c489c489d0623c390629c5f4781e264167253378
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2025-06-17 19:59:11 +0530

    Create JournalApplication.java

---
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -0,0 +1,16 @@
+package net.engineeringdigest.journalApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class JournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalApplication.class, args);
+    }
+} 
