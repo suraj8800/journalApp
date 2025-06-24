816b3fba36aed95929f8a37f183681421468fa49
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2025-06-24 21:11:54 +0530

    Role Base Authorization
    In this we will learn how to authorize on the base of role ADMIN.
    Only admin can access all users.

--- src/main/java/net/engineeringdigest/journalApp/configure/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/configure/SpringSecurity.java
@@ -23,6 +23,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers( "/journal/**","/public/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
