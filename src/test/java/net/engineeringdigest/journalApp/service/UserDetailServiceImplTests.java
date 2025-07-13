f5bb2ed83a71cf7a363bd63a1df0929a088b322c
suraj8800 <biswassuraj205@gmail.com>
2025-07-13 20:43:58 +0530

    Learn mockito Used to create mock objects in unit tests.
    Doesn't require the Spring context to load.
    
    Useful in pure JUnit + Mockito tests.

---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailServiceImplTests.java
@@ -0,0 +1,42 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+public class UserDetailServiceImplTests {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void loadUserByUserNameTest(){
+        when(userRepository.findByUserName(anyString()))
+                .thenReturn(User.builder()
+                        .userName("ram")
+                        .password("ehsueh")
+                        .roles(List.of())
+                        .build());
+        UserDetails user = userDetailsServiceImpl.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+ }
