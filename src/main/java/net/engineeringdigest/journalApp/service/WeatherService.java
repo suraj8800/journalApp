70c1a162fdecf34511a2079d1e6c4a2492a1bb36
suraj8800 <biswassuraj205@gmail.com>
2025-08-06 20:36:25 +0530

    How to Consume External POST APIs Effectively

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -1,7 +1,10 @@
 package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
+import net.engineeringdigest.journalApp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -19,6 +22,28 @@ public class WeatherService {
     public WeatherResponse getWheater(String city){
         String finalAPI = API.replace("CITY", city).replace("API_KEY", apiKey);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+        WeatherResponse body = response.getBody();
+        return body;
+    }
+
+    // this part is just for understanding a concept of How Consume extrenal  POST API effectively
+    // NOTE: This function does not work beacuse the weather api does not have any post api feature
+    public WeatherResponse postWheater(String city){
+        String finalAPI = API.replace("CITY", city).replace("API_KEY", apiKey);
+
+//        String requestBody = "{\n" +
+//                "  \"userName\":\"suraj\", \n" +
+//                "  \"password\": \"suraj\"\n" +
+//                "}  ";
+//        HttpEntity<String> httpEntity = new HttpEntity<>(requestBody);
+
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("key", "value");
+
+        User user = User.builder().userName("suraj").password("suraj").build();
+        HttpEntity<User> httpEntity = new HttpEntity<>(user);
+
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.POST, httpEntity, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
     }
