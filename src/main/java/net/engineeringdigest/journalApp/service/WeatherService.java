536f7a0f3a78b9a1dc98722d0101cb77164c14c5
suraj8800 <biswassuraj205@gmail.com>
2025-08-07 20:23:46 +0530

    convert @Component to @Service Coz Specialized form of @Component. Used to indicate that the class holds business logic or service layer logic.
    @Value in Spring Boot: Inject Properties Like a Pro!

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -3,16 +3,18 @@ package net.engineeringdigest.journalApp.service;
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import net.engineeringdigest.journalApp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
-    private static final String apiKey = "138feb9fa57fd9c2bd9dbdd23d41e7d6";
+
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     private  static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
